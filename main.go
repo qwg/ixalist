99c8af4272715975d058a9436c0cf0ef0eb56eba
Shoji Yoshinori <yshoji@cablenet.ne.jp>
2018-01-07 15:48:07 +0900

    修正

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// 兵士編成を表示
+// ソースを保存
+// chromeで表示
+// 表示したものを保存
+// 保存したファイルを引数で指定
 func main() {
 	var filename string
 	flag.StringVar(&filename, "string", "C:\\data\\a.html", "ixa listfile")
@@ -22,7 +27,11 @@ func main() {
 	if err != nil {
 		fmt.Print("url scarapping failed")
 	}
-	doc.Find(".line-number").Each(func(_ int, s *goquery.Selection) {
-		fmt.Println(s)
+	fmt.Printf("カード番号,名前,コスト\n")
+	doc.Find(".parameta_area").Each(func(_ int, s *goquery.Selection) {
+		fmt.Printf("%s,%s,%s\n",
+			s.Find(".ig_card_cardno").Text(),
+			s.Find(".ig_card_name").Text(),
+			s.Find(".ig_card_cost").Text())
 	})
 }
