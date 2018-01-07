51c3f78b86cc8678007131dd1234f103ebe8de66
Shoji Yoshinori <yshoji@cablenet.ne.jp>
2018-01-07 16:44:47 +0900

    修正

--- main.go
+++ main.go
@@ -27,11 +27,37 @@ func main() {
 	if err != nil {
 		fmt.Print("url scarapping failed")
 	}
-	fmt.Printf("カード番号,名前,コスト\n")
+	fmt.Printf("カード番号,名前,コスト,槍\n")
 	doc.Find(".parameta_area").Each(func(_ int, s *goquery.Selection) {
-		fmt.Printf("%s,%s,%s\n",
+		yari := "E"
+		if s.Find(".yari.lv_d").Size() == 1 {
+			yari = "D"
+		} else if s.Find(".yari.lv_c").Size() == 1 {
+			yari = "C"
+		} else if s.Find(".yari.lv_b").Size() == 1 {
+			yari = "B"
+		} else if s.Find(".yari.lv_a").Size() == 1 {
+			yari = "A"
+		} else if s.Find(".yari.lv_s").Size() == 1 {
+			yari = "S"
+		} else if s.Find(".yari.lv_ss").Size() == 1 {
+			yari = "SS"
+		} else if s.Find(".yari.lv_sss").Size() == 1 {
+			yari = "SSS"
+		}
+		fmt.Printf("%s,%s,%s,%s\n",
 			s.Find(".ig_card_cardno").Text(),
 			s.Find(".ig_card_name").Text(),
-			s.Find(".ig_card_cost").Text())
+			s.Find(".ig_card_cost").Text(), yari)
 	})
+}
+
+func tekisei(s *goquery.Selection, kind string) string {
+	level := []string{"sss", "ss", "s", "a", "b", "c", "d", "e"}
+	for _, l := range level {
+		if s.Find("."+kind+".lv_"+l) == 1 {
+			return strings.ToUpper(l)
+		}
+	}
+	return ""
 }
